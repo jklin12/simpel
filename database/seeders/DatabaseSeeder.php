a6790fa4b254ff8f4a9f8328492acaa4bf447eae
faris aizy <farisaizy12@gmail.com>
2026-04-06 01:41:09 +0700

    update seedert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,15 +13,26 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            // Core Infrastructure
             MasterLocationSeeder::class,
             UserRoleSeeder::class,
             LurahSeeder::class,
             NotificationSeeder::class,
+            SuratCounterSeeder::class,
+            PekerjaanSeeder::class,
+
+            // Letter Definitions & Workflows
+            JenisSuratSeeder::class,
+            ApprovalFlowSeeder::class,
+
+            // Letter Specific Configuration (Fields, OCR, PDF Templates)
             SkpSeeder::class,
             SkmhSeeder::class,
             SkjdSeeder::class,
             SksiSeeder::class,
             SkgSeeder::class,
+            SdnhSeeder::class,
+            SktmrSeeder::class,
         ]);
     }
 }
